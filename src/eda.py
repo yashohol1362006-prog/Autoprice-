fdc37c9888af1b185102441c3606b5fee49eaf6c
yash_ohol <yashohol1362006@gmail.com>
2026-09-25 23:10:13 +0530

    Correct LinearRegression import in eda.py
    Fixed import statement for LinearRegression and removed parentheses.

--- src/eda.py
+++ src/eda.py
@@ -159,6 +159,7 @@ y = df_cleaned['price']
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(x, y,test_size=0.2, random_state=42 )
 
+from sklearn.linear_model import LinearRegression()
 model = LinearRegression()
 model.fit(X_train, y_train)
 
